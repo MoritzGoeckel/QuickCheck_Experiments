d1e9ae018dc90c9e605ce8a094ac5c55e64668f8
Moritz Goeckel <moritz.goeckel@gmail.com>
2018-12-01 14:04:34 +0100

    Added feedback and did the presentation

--- Examples/9_buggysum.hs
+++ Examples/9_buggysum.hs
@@ -2,9 +2,9 @@ import Test.QuickCheck
 
 buggySum :: [Int] -> Int
 buggySum xs = 
-    if not ((elem 7 xs) && (elem 30 xs))
-        then sum xs
-        else (sum xs) - 1
+    if (elem 7 xs) && (elem 30 xs)
+        then (sum xs) - 1
+        else sum xs
 
 prop_sum :: [Int] -> Bool
 prop_sum xs = 
